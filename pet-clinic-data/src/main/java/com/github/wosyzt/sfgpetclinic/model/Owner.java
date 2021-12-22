277603e9971323278dd5e1c8c855bd4e5dbcd270
wosyzt <wosyzt@outlook.com>
2021-12-22 16:30:46 +0800

    Added Visit. update owner with pet relationship. Closes #27

--- pet-clinic-data/src/main/java/com/github/wosyzt/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/github/wosyzt/sfgpetclinic/model/Owner.java
@@ -1,9 +1,20 @@
 package com.github.wosyzt.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person{
     private String address;
     private String city;
     private String telephone;
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 
     public String getAddress() {
         return address;
